f272f6d1e9433bde8811249d43f8f5de8d0f35a7
MohammadHadian <hadiyanmohamad131@gmail.com>
2023-10-22 10:50:44 +0330

    feat: create shaders and pipeline to draw cell

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ async function main() {
   const context = canvas.getContext('webgpu');
   context.configure({
     device,
-    format: canvasFormat, //"bgra8unorm"
+    format: canvasFormat,
   });
 
   const encoder = device.createCommandEncoder();
@@ -52,6 +52,76 @@ async function main() {
     ],
   };
   const passEncoder = encoder.beginRenderPass(renderPassDescriptor);
+
+  // Define vertices
+  // prettier-ignore
+  const vertices = new Float32Array([
+    // X,   Y
+    -0.8, -0.8, // first triangle
+     0.8, -0.8,
+     0.8,  0.8,
+
+    -0.8, -0.8, // second triangle
+    -0.8,  0.8,
+     0.8,  0.8,
+  ]);
+
+  // Create vertex buffer
+  const vertexBuffer = device.createBuffer({
+    label: 'Cell vertices',
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+
+  // Copy vertex data to buffer's memory
+  device.queue.writeBuffer(vertexBuffer, /*bufferOffset=0*/ 0, vertices);
+
+  // Define vertex data structure
+  const vertexBufferLayout = {
+    arrayStride: 4 * 2,
+    attributes: [
+      {
+        format: 'float32x2',
+        shaderLocation: 0, // Position, see vertex shader
+        offset: 0,
+      },
+    ],
+  };
+
+  // Create shader module
+  const cellShaderModule = device.createShaderModule({
+    label: 'Cell shader',
+    code: `@vertex
+fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+  return vec4f(pos, 0, 1);
+}
+
+@fragment
+fn fragmentMain() -> @location(0) vec4f {
+  return vec4f(1, 0.5, 0.3, 1);
+}`,
+  });
+
+  // Cell render pipeline
+  const cellPipeline = device.createRenderPipeline({
+    label: 'Cell pipeline',
+    layout: 'auto',
+    vertex: {
+      module: cellShaderModule,
+      entryPoint: 'vertexMain',
+      buffers: [vertexBufferLayout],
+    },
+    fragment: {
+      module: cellShaderModule,
+      entryPoint: 'fragmentMain',
+      targets: [{ format: canvasFormat }],
+    },
+  });
+
+  passEncoder.setPipeline(cellPipeline);
+  passEncoder.setVertexBuffer(0, vertexBuffer);
+  passEncoder.draw(vertices.length / 2);
+
   passEncoder.end();
 
   const commandBuffer = encoder.finish();
